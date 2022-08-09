22822b58c8bed86b9f142ae699ec3824784a2528
Victor Mendes <victorutdsim7@gmail.com>
2022-08-09 18:56:20 -0300

    requisito 6: [completo] implementa testes para a função saveCartItems

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,14 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  it('testa se o metodo localStorage.setItem é chamado na função saveCartItems', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalled();
+  });
+
+  it('testa se o metodo localStorage.setItem recebe os parâmetros corretos', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', '<ol><li>Item</li></ol>');
+  });
 });
