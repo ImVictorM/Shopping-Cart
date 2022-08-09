5dc902631ccea0896f3d64891ac08f0ecc9a18ae
Victor Mendes <victorutdsim7@gmail.com>
2022-08-09 17:11:11 -0300

    requisito 4: [incompleto] implementa a função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,16 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (itemId) => {
+  try {
+    const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    error.message = 'You must provide an url';
+    return error;
+  }
 };
+
+fetchItem('MLB1615760527');
 
 if (typeof module !== 'undefined') {
   module.exports = {
