cb3ae68bc668df18da18d2a4d24894192364330d
victor mendes <victorutdsim7@gmail.com>
2024-08-23 13:58:01 -0300

    angular bootstrap

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import angularPlugin from "eslint-plugin-angular";
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.browser,
+        angular: "readonly",
+      },
+    },
+    ignores: ["node_modules", "src/app/lib"],
+    plugins: {
+      angular: angularPlugin,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      "angular/no-service-method": "error",
+      "angular/controller-as": "error",
+      "angular/di": ["error", "function"],
+      "angular/module-getter": "error",
+      "angular/module-setter": "error",
+      "angular/no-private-call": "error",
+    },
+  },
 ];
---
+++ src/app/core/layout/header/header-base.component.js
@@ -0,0 +1,5 @@
+angular
+  .module("headerBase")
+  .component("headerBase", {
+    templateUrl: "app/core/layout/header/header-base.template.html",
+  });
