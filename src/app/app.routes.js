19b20cd866f7221ea8d137c46df1358f3aa68a85
victor mendes <victorutdsim7@gmail.com>
2024-08-28 19:06:43 -0300

    refactor: handle routes in a single file

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -2,10 +2,19 @@ angular
   .module("app")
   .config(["$routeProvider", function ($routeProvider) {
     $routeProvider
+      .when("/checkout", {
+        template: "<h1>hello</h1>"
+      })
+      .when("/products", {
+        templateUrl: "app/features/products/views/products/products.template.html",
+      })
+      .when("/products/:id", {
+        templateUrl: "app/features/products/views/product-details/product-details.template.html",
+      })
       .when("/", {
         redirectTo: "/products",
       })
       .otherwise({
-        redirectTo: "/",
+        redirectTo: "/products",
       });
   }]);
